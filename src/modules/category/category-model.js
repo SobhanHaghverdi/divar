7abd692ed30a9c907def99e0a5f5400686ff0217
SobhanHaghverdi <sobhanhv.dev@gmail.com>
2024-10-24 12:26:10 +0330

    Added category model

---
+++ src/modules/category/category-model.js
@@ -0,0 +1,27 @@
+import { model, Schema, Types } from "mongoose";
+
+const CategorySchema = new Schema(
+  {
+    parent: { type: Types.ObjectId, ref: "Category" },
+    name: { type: String, required: true, trim: true },
+    parents: { type: [Types.ObjectId], ref: "Category", default: undefined },
+    slug: {
+      type: String,
+      trim: true,
+      index: true,
+      required: true,
+      lowercase: true,
+    },
+  },
+  { strict: true, virtuals: true, versionKey: false, id: false }
+);
+
+CategorySchema.virtual("children", {
+  ref: "Category",
+  localField: "_id",
+  foreignField: "parent",
+});
+
+const Category = model("Category", CategorySchema);
+
+export default Category;
