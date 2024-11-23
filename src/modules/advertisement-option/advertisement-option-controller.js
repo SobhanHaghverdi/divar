ea49bdb5c8e5fc4bedee90a7631a97852c7f5fe1
SobhanHaghverdi <sobhanhv.dev@gmail.com>
2024-11-23 18:14:29 +0330

    Add create advertisement option method

--- src/modules/advertisement-option/advertisement-option-controller.js
+++ src/modules/advertisement-option/advertisement-option-controller.js
@@ -1,4 +1,5 @@
 import autoBind from "../../common/utils/auto-bind.js";
+import AdvertisementOptionMessage from "./advertisement-option-message.js";
 import AdvertisementOptionService from "./advertisement-option-service.js";
 
 class AdvertisementOptionController {
@@ -15,7 +16,13 @@ class AdvertisementOptionController {
 
   async getByCategory(req, res) {}
 
-  async create(req, res) {}
+  async create(req, res) {
+    await this.#service.create(req.body);
+
+    return res
+      .status(201)
+      .json({ message: AdvertisementOptionMessage.CreatedSuccessfully });
+  }
 }
 
 export default new AdvertisementOptionController();
